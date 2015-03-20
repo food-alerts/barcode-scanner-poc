473d0fa48c5faf4cb5fdffa273fcb29ec4f45797
mgazanayi <marouane.gazanayi@gmail.com>
2015-03-20 23:06:27 +0100

    It works!!!!!!!
            (\(\_
         _.o o`\\\\
        ("_     ))))
         '---'  ((((          .=,
             )   )))___.-""-./=;\\\
            /    (((         \  ))))
           ;                  | |//
           /\          |      | ((
          (  \    /__.-'\    /   )
         / /` |  /    \  \  |
        / /   \ ;      \ (\ (
        \\_    ||       || ||
         \_]   ||       || ||
              /_<      /_</_<

---
+++ app/src/main/java/com/example/mgazanayi/barcodescanner/BarcodeScannerActivity.java
@@ -0,0 +1,67 @@
+package com.example.mgazanayi.barcodescanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import javax.xml.datatype.Duration;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+
+public class BarcodeScannerActivity extends Activity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_barcode_scanner, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Toast.makeText(this, result.getText(), Toast.LENGTH_LONG).show();
+    }
+}
